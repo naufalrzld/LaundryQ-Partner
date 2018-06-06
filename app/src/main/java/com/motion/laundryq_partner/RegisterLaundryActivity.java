423dc65bc7f2a115b412ff5279f0cff112d77bfc
naufalrzld <naufalrzld@gmail.com>
2018-06-06 18:22:39 +0700

    Add Map

--- app/src/main/java/com/motion/laundryq_partner/RegisterLaundryActivity.java
+++ app/src/main/java/com/motion/laundryq_partner/RegisterLaundryActivity.java
@@ -1,13 +1,23 @@
 package com.motion.laundryq_partner;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.kofigyan.stateprogressbar.StateProgressBar;
 import com.motion.laundryq_partner.adapter.ViewPagerAdapter;
 import com.motion.laundryq_partner.fragment.FinishRegisLaundryFragment;
@@ -24,10 +34,15 @@ public class RegisterLaundryActivity extends AppCompatActivity {
     StateProgressBar step;
     @BindView(R.id.tv_step_title)
     TextView tvStepTitle;
+    @BindView(R.id.img_search)
+    ImageView imgSearch;
     @BindView(R.id.viewpager)
     ViewPager viewPager;
     @BindView(R.id.btn_next)
     Button btnNext;
+
+    private static final String TAG = RegisterLaundryActivity.class.getSimpleName();
+    public static final int MAP_REQUEST_CODE = 125;
 
     private ViewPagerAdapter viewPagerAdapter;
     private int viewPagerPosition, currentState;
@@ -56,6 +71,10 @@ public class RegisterLaundryActivity extends AppCompatActivity {
 
             @Override
             public void onPageSelected(int position) {
+                if (position != 0) {
+                    imgSearch.setVisibility(View.GONE);
+                }
+
                 if (position == 3) {
                     btnNext.setText("Selesai");
                 }
@@ -64,6 +83,21 @@ public class RegisterLaundryActivity extends AppCompatActivity {
             @Override
             public void onPageScrollStateChanged(int state) {
 
+            }
+        });
+
+        imgSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                                    .build(RegisterLaundryActivity.this);
+                    startActivityForResult(intent, MAP_REQUEST_CODE);
+                } catch (GooglePlayServicesRepairableException e) {
+                    // TODO: Handle the error.
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // TODO: Handle the error.
+                }
             }
         });
 
@@ -114,4 +148,11 @@ public class RegisterLaundryActivity extends AppCompatActivity {
         setStepTitle((String) viewPagerAdapter.getPageTitle(position + 1));
         viewPager.setCurrentItem(position + 1);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Fragment fragment = viewPagerAdapter.getItem(viewPagerPosition);
+        fragment.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
--- app/src/main/java/com/motion/laundryq_partner/fragment/LocationLaundryFragment.java
+++ app/src/main/java/com/motion/laundryq_partner/fragment/LocationLaundryFragment.java
@@ -1,19 +1,74 @@
 package com.motion.laundryq_partner.fragment;
 
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.motion.laundryq_partner.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+import static com.motion.laundryq_partner.RegisterLaundryActivity.MAP_REQUEST_CODE;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LocationLaundryFragment extends Fragment {
+public class LocationLaundryFragment extends Fragment implements OnMapReadyCallback {
+    @BindView(R.id.map_view)
+    MapView mapView;
+    @BindView(R.id.til_alamat)
+    TextInputLayout tilAlamat;
+    @BindView(R.id.til_alamat_detail)
+    TextInputLayout tilAlamatDetail;
+    @BindView(R.id.et_alamat)
+    TextInputEditText etAlamat;
+    @BindView(R.id.et_alamat_detail)
+    TextInputEditText etAlamatDetail;
 
+    private static final String TAG = LocationLaundryFragment.class.getSimpleName();
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    private boolean locationPermissionGranted = false;
+
+    private GoogleMap mGoogleMap;
+    private Marker m;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     public LocationLaundryFragment() {
         // Required empty public constructor
@@ -24,7 +79,117 @@ public class LocationLaundryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location_laundry, container, false);
+        View v = inflater.inflate(R.layout.fragment_location_laundry, container, false);
+        ButterKnife.bind(this, v);
+
+        getLocationPermission();
+
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
+
+        return v;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        Toast.makeText(getContext(), "Map Is Ready", Toast.LENGTH_SHORT).show();
+        if (locationPermissionGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mGoogleMap.setMyLocationEnabled(true);
+        }
+    }
+
+    public void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "moveCamera: moving the camera to: lat " + latLng.latitude + ", lng: " + latLng.longitude);
+
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+    }
+
+    public void addMark(LatLng latLng, String address) {
+        m = mGoogleMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title(address));
+        m.setTag(0);
+    }
+
+    private void getDeviceLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        try {
+            if (locationPermissionGranted) {
+                fusedLocationProviderClient.getLastLocation()
+                        .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    moveCamera(new LatLng(location.getLatitude(), location.getLongitude()), DEFAULT_ZOOM);
+                                }
+                            }
+                        });
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException" + e.getMessage());
+        }
+    }
+
+    private void getLocationPermission() {
+        String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), permission, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            locationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    locationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MAP_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(getContext(), data);
+                moveCamera(place.getLatLng(), DEFAULT_ZOOM);
+                if (m != null) {
+                    m.remove();
+                }
+                addMark(place.getLatLng(), String.valueOf(place.getAddress()));
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(getContext(), data);
+                // TODO: Handle the error.
+                Log.i(TAG, status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
 }
